5baffeb9bc7f91a5acdf55018e6b28c1cd24ea14
Regis Freyd <djaiss@users.noreply.github.com>
2023-12-14 22:38:51 -0500

    feat: sidebar

--- resources/views/names/partials/sidebar.blade.php
+++ resources/views/names/partials/sidebar.blade.php
@@ -1,14 +1,14 @@
-<ul>
-  <li>
+<ul class="space-y-1">
+  <li class="p-2 border {{ request()->routeIs('name.index') ? 'bg-yellow-100 border-yellow-200' : 'border-transparent' }} hover:bg-yellow-100 hover:border-yellow-200 rounded-md">
     <a href="{{ route('name.index') }}">Tous les prénoms</a>
   </li>
-  <li>
+  <li class="p-2 border {{ request()->routeIs('name.garcon.index') ? 'bg-yellow-100 border-yellow-200' : 'border-transparent' }} hover:bg-yellow-100 hover:border-yellow-200 rounded-md">
     <a href="{{ route('name.garcon.index') }}">Prénoms masculins</a>
   </li>
-  <li>
+  <li class="p-2 border {{ request()->routeIs('name.fille.index') ? 'bg-yellow-100 border-yellow-200' : 'border-transparent' }} hover:bg-yellow-100 hover:border-yellow-200 rounded-md">
     <a href="{{ route('name.fille.index') }}">Prénoms féminins</a>
   </li>
-  <li>
+  <li class="p-2 border {{ request()->routeIs('name.') ? 'bg-yellow-100 border-yellow-200' : 'border-transparent' }} hover:bg-yellow-100 hover:border-yellow-200 rounded-md">
     <a href="{{ route('name.garcon.index') }}">Prénoms mixtes</a>
   </li>
 </ul>
