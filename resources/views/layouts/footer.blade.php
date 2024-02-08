aed7e96ab206b477df9a1eda8de4cc2cd1a71d8d
Mazarin <djaiss@users.noreply.github.com>
2024-02-08 07:14:35 -0500

    feat: add terms of use in footer (#27)

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,10 @@
 <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
   <div class="flex items-center py-4 justify-between text-xs">
-    © 2023-{{ now()->year }}
+    <div class="mr-10">
+      © 2023-{{ now()->year }}
+    </div>
+    <div>
+      <a href="{{ route('terms.index') }}" class="underline">Conditions d'utilisation</a>
+    </div>
   </div>
 </div>
