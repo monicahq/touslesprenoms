0d1d5d6d71247fa125675fe8c8dc38e9882d3a4a
Regis Freyd <djaiss@users.noreply.github.com>
2024-02-12 20:29:14 -0500

    wip

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -2,5 +2,5 @@
 
 <input {{ $disabled ? 'disabled' : '' }}
        {!! $attributes->merge([
-           'class' => 'flex h-9 w-full rounded-md dark:bg-gray-900 border border-gray-300 dark:border-gray-600 bg-transparent px-3 py-1 shadow-sm transition-colors placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 bg-white',
+           'class' => 'flex h-9 w-full rounded-md border border-gray-300 bg-transparent px-3 py-1 shadow-sm transition-colors placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 bg-white',
        ]) !!}>
