5976791b5cc12e1a89856bf610b4546e822b736d
Alexis Saettler <alexis@saettler.org>
2024-02-18 21:08:10 +0100

    fix: fix sitemap generate (#46)

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -4,7 +4,11 @@ namespace App\Console\Commands;
 
 use App\Models\Name;
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\SitemapIndex;
+use Spatie\Sitemap\Tags\Sitemap as SitemapTag;
 use Spatie\Sitemap\Tags\Url;
 
 class GenerateSitemap extends Command
@@ -28,21 +32,32 @@ class GenerateSitemap extends Command
      */
     public function handle(): void
     {
-        $sitemap = Sitemap::create();
+        $sitemapIndex = SitemapIndex::create();
 
         Name::where('name', '!=', '_PRENOMS_RARES')
-            ->get()
-            ->each(function (Name $name) use ($sitemap) {
-                $sitemap->add(
-                    Url::create(route('name.show', [
-                        'id' => $name->id,
-                        'name' => $name->name,
-                    ]))
-                        ->setPriority(0.9)
-                        ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
-                );
-            });
+            ->chunkById(2000, function (Collection $names, int $key) use ($sitemapIndex) {
 
-        $sitemap->writeToFile(public_path('sitemap.xml'));
+                $file = 'sitemap_' . Str::padLeft($key, 2, '0') . '.xml';
+                $sitemap = Sitemap::create();
+
+                $names->each(function (Name $name) use ($sitemap) {
+                    $sitemap->add(
+                        Url::create(route('name.show', [
+                            'id' => $name->id,
+                            'name' => Str::lower($name->name),
+                        ]))
+                            ->setPriority(0.9)
+                            ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
+                            ->setLastModificationDate($name->updated_at)
+                    );
+                });
+
+                $sitemap->writeToFile(public_path($file));
+                $sitemap = null;
+
+                $sitemapIndex->add(SitemapTag::create(url($file)));
+            }, 'id');
+
+        $sitemapIndex->writeToFile(public_path('sitemap.xml'));
     }
 }
