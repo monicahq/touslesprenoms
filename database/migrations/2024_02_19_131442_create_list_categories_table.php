72dd088bafda5d1f91446d0f59763595d67a77ee
Regis Freyd <djaiss@users.noreply.github.com>
2024-02-19 08:15:30 -0500

    feat: add categories for lists

---
+++ database/migrations/2024_02_19_131442_create_list_categories_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('list_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+
+
+    }
+};
