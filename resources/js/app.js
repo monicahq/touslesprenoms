acbed8e1d0a670e3aaea70caaf16217f36f8ead5
Alexis Saettler <alexis@saettler.org>
2024-02-24 22:20:43 +0100

    chore: use prebuilt image (#63)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,9 @@
 import './bootstrap';
+import '../css/app.css';
 
 import Alpine from 'alpinejs';
 import Clipboard from "@ryangjchandler/alpine-clipboard";
 import htmx from 'htmx.org';
-import 'charts.css';
 
 window.Alpine = Alpine;
 window.htmx = htmx;
